a786ef9935e905afc8e4d6b66817117148a5b1ad
Alexey Parshin <aparshin01@mail.ru>
2022-08-29 17:23:30 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect 
 from django.utils import timezone
 from blog.models import Post
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect
 from blog.forms import CommentForm
 import logging
+# from django.views.decorators.cache import cache_page
+# from django.views.decorators.vary import vary_on_cookie
 
 logger = logging.getLogger(__name__)
 
